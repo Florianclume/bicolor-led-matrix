b10de8f8122d5d9427ffb9fa583696ead6402d9c
Florianclume <90716018+Florianclume@users.noreply.github.com>
2022-03-06 21:16:51 +0100

    Add persistence input parameter

--- emulator/index.js
+++ emulator/index.js
@@ -2,45 +2,50 @@ import * as avr8js from './avr8js/index.js';
 import { SN74HC595 } from './sn74hc595.js';
 import { loadHex, buildHex } from './builder.js';
 
-const persist = 100;
-
+var persistence = 100;
 var stop = false;
 
 const codeArduino = document.getElementById('codeArduino');
 const runButton = document.getElementById('runButton');
 const stopButton = document.getElementById('stopButton');
 const compilingOutput = document.getElementById('compilingOutput');
+const persistenceRange = document.getElementById('persistenceRange');
+const persistenceRangeValue = document.getElementById('persistenceRangeValue');
 const display = document.getElementById('display');
-const matrix = new Array(64).fill(undefined).map((_, i) => display.querySelector(`#r${Math.floor(i / 8) + 1}c${i % 8 + 1}`))
+const matrix = new Array(64).fill(undefined).map((_, i) => display.querySelector(`#r${Math.floor(i / 8) + 1}c${i % 8 + 1}`));
 
-const colors = ['grey', 'lime', 'red', 'orange']
-const state = new Array(64).fill(0);
-const timeouts = new Array(64).fill(null);
-
-function updateMatrix(cathods, greens, reds) {
-    state.forEach((previous, i) => {
-        const next = (!cathods[Math.floor(i / 8)] && greens[i % 8] + reds[i % 8] * 2) + 0 // dirty :)
-        if (next != previous) {
-            if (timeouts[i] !== null) {
-                clearTimeout(timeouts[i]);
-                timeouts[i] = null;
-            }
-            if (next == 0) {
-                timeouts[i] = setTimeout(() => matrix[i].setAttribute('fill', colors[next]), persist);
-            } else {
-                matrix[i].setAttribute('fill', colors[next]);
-            }
-        }
-        state[i] = next
-    });
-}
-
+persistenceRange.addEventListener('input', () => {
+    persistence = parseInt(persistenceRange.value, 10);
+    persistenceRangeValue.textContent = persistence + 'ms';
+});
 runButton.addEventListener('click', () => {
     run(codeArduino.value);
 });
 stopButton.addEventListener('click', () => {
     stop = true;
 });
+
+const colors = ['grey', 'lime', 'red', 'orange'];
+const state = new Array(64).fill(0);
+const timeouts = new Array(64).fill(null);
+
+function updateMatrix(cathods, greens, reds) {
+    state.forEach((previous, i) => {
+        const next = (!cathods[Math.floor(i / 8)] && greens[i % 8] + reds[i % 8] * 2) + 0; // dirty :)
+        if (next != previous) {
+            if (timeouts[i] !== null) {
+                clearTimeout(timeouts[i]);
+                timeouts[i] = null;
+            }
+            if (next == 0) {
+                timeouts[i] = setTimeout(() => matrix[i].setAttribute('fill', colors[next]), persistence);
+            } else {
+                matrix[i].setAttribute('fill', colors[next]);
+            }
+        }
+        state[i] = next;
+    });
+}
 
 async function run(code) {
     stop = true;
@@ -81,7 +86,7 @@ async function run(code) {
             setTimeout(runCode, 0);
         } else {
             stop = false;
-            console.log('STOPPED !')
+            console.log('STOPPED !');
         }
     }
 
